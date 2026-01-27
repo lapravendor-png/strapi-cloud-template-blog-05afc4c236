f197ce3cc66638a8626792aab8a0a697f6fc1af6
DmytroBackend <la.pravendor@gmail.com>
2026-01-27 16:41:48 +0100

    adding collection type: competences

---
+++ src/api/competences/controllers/competence.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  competences controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::competences.competence');
---
+++ src/api/competences/routes/competence.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * competences router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::competences.competence');
