30398463d7b45b170fd3f8c2aae953d8c0163d9e
DmytroBackend <la.pravendor@gmail.com>
2026-03-02 12:51:05 +0100

    Fix

--- src/api/webinar-admins/routes/webinar-admin.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * webinar-admin router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::webinar-admins.webinar-admin');
--- src/api/webinar-admins/services/webinar-admin.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * webinar-admin service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::webinar-admins.webinar-admin');
